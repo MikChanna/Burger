338b05ebb13355e9dbfda33af4d5d51bb57b9599
MikChanna <channa.mik@gmail.com>
2020-08-18 17:04:45 -0400

    created schema and seed file

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ("Cheeseburger", false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ("Teriyaki Burger", false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ("Veggie Burger", false);
