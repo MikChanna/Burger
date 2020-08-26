89fecb6d9e7c70adfe6ad47f73c321ded479f958
MikChanna <channa.mik@gmail.com>
2020-08-26 18:43:42 -0400

    added heroku JAWDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,25 @@
 var mysql = require("mysql");
 // var pw = require("../pw");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db",
-});
+if ((process.env, JAWSDB_URL)) {
+  commection = mysqul.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
+
+// connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db",
+// });
 
 // Make connection.
 connection.connect(function (err) {
