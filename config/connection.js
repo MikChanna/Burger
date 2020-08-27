68033a4575f98988b41558617d24fc228b637f18
MikChanna <channa.mik@gmail.com>
2020-08-26 21:40:49 -0400

    set up JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if ((process.env, JAWSDB_URL)) {
   commection = mysqul.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db",
+    user: "s7ffx5yrg1bs58i2",
+    password: "lq6wh0ocv60kcisv",
+    database: "ryh0pf6qa6x9bv7p",
   });
 }
 
